df6db8a16e52615de20a046e8b72ef984997a649
bodnarb.04 <bodnarb.04@gmail.com>
2022-10-17 12:08:59 +0200

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SouboryBin07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("../../../sport.dat", FileMode.Create);
+            StreamReader sr = new StreamReader(fs);
+            BinaryWriter be = new BinaryWriter(fs);
+        }
+    }
+}
